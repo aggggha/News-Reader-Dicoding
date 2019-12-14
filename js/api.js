c8801cf1dd7618307cf3f352affaaf79b6a1d75a
Agha Pradipta <pradipta.agha@gmail.com>
2019-12-14 21:32:41 +0700

    Added readme.md and fixed some bugs

--- js/api.js
+++ js/api.js
@@ -80,17 +80,46 @@ function getArticles() {
 }
 
 function getArticleById() {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
         // Ambil query param (id)
         var urlParams = new URLSearchParams(window.location.search);
         var idParam = urlParams.get("id");
 
-        if("caches" in window) {
-        caches.match(base_url + "article/" + idParam).then(function (response) {
-            if (response) {
-                response.json().then(function (data) {
-                    console.log(data);
-                    var articleHTML = `
+        if ("caches" in window) {
+            caches.match(base_url + "article/" + idParam).then(function (response) {
+                if (response) {
+                    response.json().then(function (data) {
+                        console.log(data);
+                        var articleHTML = `
+                            <div class="card">
+                                <div class="card-image waves-effect waves-block waves-light">
+                                    <img src="${data.result.cover}" />
+                                </div>
+                                <div class="card-content">
+                                    <span class="card-title">${data.result.post_title}</span>
+                                    ${snarkdown(data.result.post_content)}
+                                </div>
+                            </div>
+                        `;
+
+                        // Sisipkan ke elemen #content
+                        document.getElementById("body-content").innerHTML = articleHTML;
+
+                        // kirim hasil parsing json
+                        resolve(data);
+                    });
+                }
+            });
+        }
+
+        fetch(base_url + "article/" + idParam)
+            .then(status)
+            .then(json)
+            .then(function (data) {
+                // Objek dari respons.json()
+                console.log(data);
+                // Menyusun card
+                var articleHTML = `
                     <div class="card">
                         <div class="card-image waves-effect waves-block waves-light">
                             <img src="${data.result.cover}" />
@@ -102,40 +131,60 @@ function getArticleById() {
                     </div>
                 `;
 
-                    // Sisipkan ke elemen #content
-                    document.getElementById("body-content").innerHTML = articleHTML;
+                // Sisipkan ke elemen #content
+                document.getElementById("body-content").innerHTML = articleHTML;
 
-                    // kirim hasil parsing json
-                    resolve(data);
-                });
-            }
+                // Kirim hasil parsing json
+                resolve(data);
+            });
+    });
+}
+
+function getSavedArticles() {
+    getAll().then(function (articles) {
+        console.log(articles);
+
+        // Card article
+        var articlesHTML = "";
+        articles.forEach(function (article) {
+            var description = article.post_content.substring(0, 100);
+            articlesHTML += `
+                <div class="card">
+                    <a href="./article.html?id=${article.ID}&saved=true">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img src="${article.cover}" />
+                    </div>
+                    </a>
+                    <div class="card-content">
+                    <span class="card-title truncate">${article.post_title}</span>
+                    <p>${description}</p>
+                    </div>
+                </div>
+            `;
         });
-    }
 
-    fetch(base_url + "article/" + idParam)
-        .then(status)
-        .then(json)
-        .then(function (data) {
-            // Objek dari respons.json()
-            console.log(data);
-            // Menyusun card
-            var articleHTML = `
+        document.getElementById("body-content").innerHTML = articlesHTML;
+    });
+}
+
+function getSavedArticleById() {
+    var urlParams = new URLSearchParams(window.location.search);
+    var idParam = urlParams.get("id");
+
+    getById(idParam).then(function (article) {
+        articleHTML = '';
+        var articleHTML = `
             <div class="card">
                 <div class="card-image waves-effect waves-block waves-light">
-                    <img src="${data.result.cover}" />
+                <img src="${article.cover}" />
                 </div>
                 <div class="card-content">
-                    <span class="card-title">${data.result.post_title}</span>
-                    ${snarkdown(data.result.post_content)}
+                <span class="card-title">${article.post_title}</span>
+                ${snarkdown(article.post_content)}
                 </div>
             </div>
         `;
-
-            // Sisipkan ke elemen #content
-            document.getElementById("body-content").innerHTML = articleHTML;
-
-            // Kirim hasil parsing json
-            resolve(data);
-        });
+        // Sisipkan komponen card ke dalam elemen dengan id #content
+        document.getElementById("body-content").innerHTML = articleHTML;
     });
 }
--- js/db.js
+++ js/db.js
@@ -1,5 +1,5 @@
 var dbPromised = idb.open("news-reader", 1, function (upgradeDb) {
-    var articlesObjectStore = upgradeDb.createObjectStore("articles", { keyPath: "id" });
+    var articlesObjectStore = upgradeDb.createObjectStore("articles", { keyPath: "ID" });
     articlesObjectStore.createIndex("post_title", "post_title", { unique: false });
 });
 
@@ -31,29 +31,16 @@ function getAll() {
     });
 }
 
-function getSavedArticles() {
-    getAll().then(function (articles) {
-        console.log(articles);
-
-        // Menyusn card
-        var articlesHTML = "";
-        articles.forEach(function (article) {
-            var description = article.post_content.substring(0, 100);
-            articlesHTML += `
-                <div class="card">
-                    <a href="./article.html?id=${article.ID}">
-                        <div class="card-image waves-effect waves-block waves-light">
-                            <img src="${article.cover}" />
-                        </div>
-                    </a>
-                    <div class="card-content">
-                        <span class="card-title truncate">${article.post_title}</span>
-                        <p>${description}</p>
-                    </div>
-                </div>
-            `;
+function getById(id) {
+    return new Promise(function (resolve, reject) {
+        dbPromised
+        .then(function (db) {
+            var tx = db.transaction("articles", "readonly");
+            var store = tx.objectStore("articles");
+            return store.get(id);
+        })
+        .then(function (article) {
+            resolve(article); 
         });
-
-        document.getElementById("body-content").innerHTML - articlesHTML;
     });
 }
